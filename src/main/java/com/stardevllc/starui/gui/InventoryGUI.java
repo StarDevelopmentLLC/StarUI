7ecf21b2f595da512d861be66fcb4cb04fad4aee
Firestar311 <slaterandrew0398@gmail.com>
2025-02-16 15:05:42 -0600

    Removed the delete on update check in the update methods

--- src/main/java/com/stardevllc/starui/gui/InventoryGUI.java
+++ src/main/java/com/stardevllc/starui/gui/InventoryGUI.java
@@ -16,7 +16,7 @@ import org.bukkit.inventory.Inventory;
 import java.util.UUID;
 
 public class InventoryGUI implements InventoryHandler {
-    
+
     protected final Inventory inventory;
     protected final Slot[] slots;
     protected final int rows;
@@ -28,33 +28,33 @@ public class InventoryGUI implements InventoryHandler {
         if (rows < 1 || rows > 6) {
             throw new IllegalArgumentException("Rows value is out of range, it can only be between 1 to 6.");
         }
-        
+
         this.inventory = Bukkit.createInventory(null, rows * 9, ChatColor.translateAlternateColorCodes('&', title));
-        
+
         this.slots = new Slot[rows * 9];
         for (int i = 0; i < this.slots.length; i++) {
             slots[i] = new Slot(i);
         }
     }
-    
+
     public InventoryGUI(int rows, String title) {
         this(rows, title, null);
     }
-    
+
     public void createItems() {
-        
+
     }
-    
+
     public void update() {
         if (this.playerUUID == null) {
             return;
         }
-        
+
         Player player = Bukkit.getPlayer(this.playerUUID);
         if (player == null) {
             return;
         }
-        
+
         for (Slot slot : this.slots) {
             if (slot.getElement() != null) {
                 if (!slot.getElement().isDeleteOnUpdate()) {
@@ -63,7 +63,7 @@ public class InventoryGUI implements InventoryHandler {
             }
             slot.setElement(null);
         }
-        
+
         createItems();
         decorate(player);
     }
@@ -95,16 +95,16 @@ public class InventoryGUI implements InventoryHandler {
             }
         }
     }
-    
+
     private void setSlotValue(int row, int column, Element value) {
         if (row < 0 || row >= this.rows) {
             throw new IllegalArgumentException("Invalid row value. Supports 0-" + (this.rows - 1));
         }
-        
+
         if (column < 0 || column > 8) {
             throw new IllegalArgumentException("Invalid column value. Supports 0-8");
         }
-        
+
         Slot slot = slots[row * 9 + column];
         slot.setElement(value);
     }
@@ -113,9 +113,7 @@ public class InventoryGUI implements InventoryHandler {
         for (int index = 0; index < this.slots.length; index++) {
             Element element = slots[index].getElement();
             if (element != null) {
-                if (!element.isDeleteOnUpdate()) {
-                    this.inventory.setItem(index, element.getIconCreator().apply(player));
-                }
+                this.inventory.setItem(index, element.getIconCreator().apply(player));
             } else {
                 this.inventory.setItem(index, null);
             }
@@ -125,13 +123,13 @@ public class InventoryGUI implements InventoryHandler {
     public Inventory getInventory() {
         return inventory;
     }
-    
+
     @Override
     public void onClick(InventoryClickEvent e) {
         if (e.getRawSlot() < 0) {
             return;
         }
-        
+
         if (e.getSlot() != e.getRawSlot()) {
             return;
         }
@@ -143,18 +141,18 @@ public class InventoryGUI implements InventoryHandler {
         if (this.slots.length < e.getRawSlot()) {
             return;
         }
-        
+
         e.setCancelled(true); //TODO This will have to be changed when the InsertElement is implemented
-        
+
         Slot slot = this.slots[e.getRawSlot()];
         if (slot == null) {
             return;
         }
-        
+
         if (slot.getElement() == null) {
             return;
         }
-        
+
         if (slot.getElement() instanceof Button button) {
             if (button.getEventConsumer() != null) {
                 button.getEventConsumer().accept(e);
@@ -162,19 +160,19 @@ public class InventoryGUI implements InventoryHandler {
             }
         }
     }
-    
+
     @Override
     public void onDrag(InventoryDragEvent e) {
-        
+
     }
-    
+
     @Override
     public void onOpen(InventoryOpenEvent e) {
         decorate((Player) e.getPlayer());
     }
-    
+
     @Override
     public void onClose(InventoryCloseEvent e) {
-        
+
     }
 }
